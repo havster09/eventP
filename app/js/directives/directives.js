01ea3d7b8cf0a181613432bf39e8874ba9758284
Haven.Ramos <havster09@gmail.com>
2015-07-21 12:06:03 +1000

    tues

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -42,3 +42,120 @@ angular.module("eventsApp").directive("vote",function(){
     }
 
 });
+
+angular.module("eventsApp").directive("mybday",function(){
+   return{
+       restrict:"A",
+       link:function(scope,el,attrs,controller){
+           el.on('keydown',function(event){
+               console.log(event.keyCode);
+               if(event.keyCode == 57){
+                   return true;
+               }
+               else{
+                   return false;
+               }
+           })
+       }
+   }
+});
+
+angular.module("eventsApp").controller("greetingCtrl",function($scope){
+    $scope.greetings = ['hello'];
+    $scope.sayHello = function(){
+        alert($scope.greetings);
+    };
+    $scope.addGreeting = function(greeting){
+        $scope.greetings.push(greeting);
+    };
+});
+
+angular.module("eventsApp").directive("greeting",function(){
+    return{
+        restrict:"E",
+        replace:true,
+        priority:1,
+        transclude:true,
+        template:'<div><button class="btn btn-success" ng-click="sayHello()">Say Hello</button><div ng-transclude></div></div>',
+        controller:function($scope){
+            $scope.greetings = ['hello'];
+            $scope.sayHello = function(){
+                alert($scope.greetings);
+            };
+            this.addGreeting = function(greeting){ //"this" so function can be shared to other directives
+                $scope.greetings.push(greeting);
+            };
+        }
+    }
+});
+
+angular.module("eventsApp").directive("tagalog",function(){
+    return{
+        restrict:"A",
+        priority:2,
+        //terminal:true,
+        require:"^greeting",
+        link:function(scope,el,attrs,controller){
+            controller.addGreeting('kamusta');
+        }
+    }
+});
+
+angular.module("eventsApp").directive("spanish",function(){
+    return{
+        restrict:"A",
+        priority:1,
+        require:"^greeting",
+        link:function(scope,el,attrs,controller){
+            controller.addGreeting('ola');
+        }
+    }
+});
+
+angular.module("eventsApp").directive("collaspsable",function(){
+    return{
+        restrict:"E",
+        template:'<h1>{{title}}</h1><button class="btn btn-info" ng-click="collapse()">Collaspse</button><div ng-show="visible" ng-transclude></div>', // {{val}} will compile into the val property
+        transclude:true,
+        scope:{
+            title:"@"
+        },
+        controller:function($scope){
+            $scope.visible = true;
+            $scope.collapse = function(){
+                $scope.visible = !$scope.visible;
+            }
+        }
+    }
+});
+
+angular.module("eventsApp").directive("repeatX",function(){
+    return{
+        restrict:"A",
+        compile:function(el,attrs){//better alternative to $compile()(scope) returns a link function
+            for(var i = 0; i < Number(attrs.repeatX)-1;i++){
+                el.after(el.clone().attr('repeat-x',0));
+            }
+            return function(scope,el,attrs,controller){
+                attrs.$observe('text',function(newValue){
+                    if(newValue === "get hot"){
+                        el.css('color','red');
+                    }
+                })
+            }
+        }
+    }
+});
+
+
+/*angular.module("eventsApp").directive("repeatX",function($compile){
+    return{
+        restrict:"A",
+        link:function(scope,el,attrs,controller){
+            for(var i = 0; i < Number(attrs.repeatX)-1;i++){
+                el.after($compile(el.clone().attr('repeat-x',0))(scope)); //$compile requires scope | $compile(magic)(scope)
+            }
+        }
+    }
+});*/
+
